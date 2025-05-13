0e1e3d0ed1893dca1f0cd3c18ac6194825291093
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-13 18:05:08 -0300

    Add files via upload

---
+++ Exercices/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isOlder = false;
+        boolean isNew = true;
+        if (isOlder) {
+            System.out.println("Older");
+            System.out.println("Velho");
+        }
+        else if (isNew) {
+            System.out.println("New");
+        }
+    }
+}

469e17024687745c2884e9d9552f28efbdbfde61
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-13 17:59:33 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        boolean isOlder = false;
+        boolean isNew = true;
+        if (isOlder) {
+            System.out.println("Older");
+            System.out.println("Velho");
+        }
+        else if (isNew) {
+            System.out.println("New");
+        }
+    }
+}

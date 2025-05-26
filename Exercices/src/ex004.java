a5d457d7d18ae9cf94381218d0d5b7800d30ea8b
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-26 19:46:11 -0300

    Update ex004.java

--- Exercices/src/ex004.java
+++ Exercices/src/ex004.java
@@ -1,6 +1,6 @@
 public class ex004 {
     public static void main(String[] args) {
-        int numero = 9876543321;
+        int numero = 98716543321;
         System.out.println(medirCaracteres(numero));
     }
     public static int medirCaracteres(int numero){

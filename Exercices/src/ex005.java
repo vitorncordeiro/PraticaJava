e6de8b74cf2fcff6619f4e3e36939e831182a204
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-26 20:21:37 -0300

    Update ex005.java

--- Exercices/src/ex005.java
+++ Exercices/src/ex005.java
@@ -12,9 +12,9 @@ public class ex005 {
     public static int calculateHighScorePosition(int playersScore) {
         if (playersScore >= 1000) {
             return 1;
-        }else if(playersScore >= 500 && playersScore < 1000) {
+        }else if(playersScore >= 500) {
             return 2;
-        }else if(playersScore >= 100 && playersScore < 500) {
+        }else if(playersScore >= 100) {
             return 3;
         }else{
             return 4;

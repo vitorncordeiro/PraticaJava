fe259eece775d6a3fff8f5f0667332177eb6d8cd
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-26 20:17:35 -0300

    Update ex005.java

--- Exercices/src/ex005.java
+++ Exercices/src/ex005.java
@@ -1,1 +1,23 @@
-
+public class ex005 {
+    public static void main(String[] args) {
+        String vitor = "Vitor";
+        int score = 130;
+        int posicao = calculateHighScorePosition(score);
+        System.out.println(displayHighScorePosition(vitor, posicao));
+    }
+    public static String displayHighScorePosition(String playersName, int playersPosition) {
+        String formatada = playersName + " managed to get into position " + playersPosition + " on the high score.";
+        return formatada;
+    }
+    public static int calculateHighScorePosition(int playersScore) {
+        if (playersScore >= 1000) {
+            return 1;
+        }else if(playersScore >= 500 && playersScore < 1000) {
+            return 2;
+        }else if(playersScore >= 100 && playersScore < 500) {
+            return 3;
+        }else{
+            return 4;
+        }
+    }
+}

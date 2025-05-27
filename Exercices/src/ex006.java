1271694ba5077eb35bf08bda86ed1bd4d3d125dd
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-27 11:39:23 -0300

    Create ex006.java

---
+++ Exercices/src/ex006.java
@@ -0,0 +1,11 @@
+public class PositiveNegativeZero {
+    public static void checkNumber(int numero){
+        if(numero > 0){
+            System.out.println("positive");
+        }else if(numero < 0){
+            System.out.println("negative");
+        }else{
+            System.out.println("zero");
+        }
+    }
+}

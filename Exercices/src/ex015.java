2b07d9fdfc12fb47156f3286c40617ff998d5e56
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-28 16:26:14 -0300

    Create ex015.java

---
+++ Exercices/src/ex015.java
@@ -0,0 +1,22 @@
+public class ex005 {
+    public static void main(String[] args) {
+        int alturaVitorOnlyInches = 74;
+        System.out.println("74 inches in cm: ");
+        convertToCentimeters(alturaVitorOnlyInches);
+        int alturaVitorFeets = 6;
+        int alturaVitorInches = 2;
+        System.out.println("6'2 in cm: ");
+        convertToCentimeters(alturaVitorFeets, alturaVitorInches);
+
+    }
+    public static double convertToCentimeters(int heightInInches) {
+        double defaultCM = 0.393701;
+        double convertido = heightInInches / defaultCM ;
+        System.out.println(convertido + "cm");
+        return convertido;
+    }
+    public static void convertToCentimeters(int heightInFeet, int heightInInches) {
+        int fullInches = (heightInFeet * 12) + heightInInches;
+        convertToCentimeters(fullInches);
+    }
+}

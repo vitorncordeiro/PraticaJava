2a58405eb17d87ae16644cfc9b735377364717c8
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-28 15:12:05 -0300

    Update ex012.java

--- Exercices/src/ex012.java
+++ Exercices/src/ex012.java
@@ -1,6 +1,12 @@
 public class DecimalComparator {
     // write code here
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
-        
+        double num1Truncado = (int) (num1 * 1000);
+        double num2Truncado = (int) (num2 * 1000);
+        if (num1Truncado == num2Truncado){
+            return true;
+        }else{
+            return false;
+        }
     }
 }

2f734b3888e621a90c9fd84a2b30e490233989f5
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-28 15:15:33 -0300

    Create ex013.java

---
+++ Exercices/src/ex013.java
@@ -0,0 +1,10 @@
+public class EqualSumChecker {
+    // write code here
+    public static boolean hasEqualSum(int x, int y, int z){
+        if (x + y == z){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

a1a36f405183e623706339adb200f21f7e465b51
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-06-26 20:20:58 -0300

    Create Carpet.java

---
+++ Exercices/ex037/Carpet.java
@@ -0,0 +1,11 @@
+public class Carpet{
+    private double cost;
+    
+    public Carpet(double cost){
+        this.cost = cost > 0 ? cost : 0;
+        
+    }
+    public double getCost(){
+        return cost;
+    }
+}

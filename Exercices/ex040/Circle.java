7f5c36943c3d70d91e6a014cafb365a3ad9a9fbc
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-22 18:01:48 -0300

    Create Circle.java

---
+++ Exercices/ex040/Circle.java
@@ -0,0 +1,16 @@
+public class Circle {
+    private double radius;
+    
+
+    
+    public Circle(double radius){
+        this.radius = radius > 0 ? radius : 0;
+    }
+    
+    public double getRadius(){
+        return radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+}

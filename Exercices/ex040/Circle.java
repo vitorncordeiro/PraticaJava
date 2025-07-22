664dee1b1b1e3f63f386e0074faa776ee062548c
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-22 18:01:01 -0300

    Update and rename Main.java to Cylinder.java

---
+++ Exercices/ex040/Circle.java
@@ -0,0 +1,18 @@
+public class Cylinder extends Circle {
+    private double height;
+    
+    public Cylinder(double radius, double height){
+        super(radius);
+        this.height = height > 0 ? height : 0;
+        
+        
+    }
+    public double getHeight(){
+        return height;
+    }
+    public double getVolume(){
+        return height * super.getArea();
+    }
+    
+    
+}

f75110023cf62d68fc0e99f3198fcba4b11e861f
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-28 15:04:48 -0300

    Update Main.java

---
+++ Exercices/ex042/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        SmartKitchen kitchen = new SmartKitchen();
+        kitchen.setKitchenState(true, true, false);
+        kitchen.doKitchenWork();
+
+
+
+    }
+}

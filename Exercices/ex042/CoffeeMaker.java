11bda2314e1c0c8c1992b35c1e5c08f626204679
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-28 15:05:41 -0300

    Create CoffeeMaker.java

---
+++ Exercices/ex042/CoffeeMaker.java
@@ -0,0 +1,17 @@
+public class CoffeeMaker {
+    private boolean hasWorkToDo;
+
+    public boolean isHasWorkToDo() {
+        return hasWorkToDo;
+    }
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+    public void brewCoffee(){
+        if(hasWorkToDo){
+            System.out.println("Brewing Coffee");
+            hasWorkToDo = false;
+        }
+    }
+}

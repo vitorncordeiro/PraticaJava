f866af91df5ac4b4bd47ff52792b79319bae68ea
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-30 20:04:42 -0300

    Update Mitsubishi.java

--- Exercices/ex045/Mitsubishi.java
+++ Exercices/ex045/Mitsubishi.java
@@ -1,1 +1,18 @@
-
+public class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name){
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine(){
+        return "Starting Engine Mitsubishi";
+    }
+    @Override
+    public String accelerate(){
+        return "Accelerating Mitsubishi";
+    }
+    @Override
+    public String brake(){
+        return "Braking Car Mitsubishi";
+    }
+    
+}

62262626a65a71225046cdb296d0be8e90ada9d2
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-30 20:04:32 -0300

    Update Holden.java

--- Exercices/ex045/Holden.java
+++ Exercices/ex045/Holden.java
@@ -1,1 +1,18 @@
 
+public class Holden extends Car{
+    public Holden(int cylinders, String name){
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine(){
+        return "Starting Engine Holden";
+    }
+    @Override
+    public String accelerate(){
+        return "Accelerating Holden";
+    }
+    @Override
+    public String brake(){
+        return "Braking Car Holden";
+    }
+}

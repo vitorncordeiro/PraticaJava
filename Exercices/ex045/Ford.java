d2f32b0c43aea0db34e8309fcaa3b107bfa84990
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-30 20:04:22 -0300

    Update Ford.java

--- Exercices/ex045/Ford.java
+++ Exercices/ex045/Ford.java
@@ -1,1 +1,17 @@
-
+public class Ford extends Car{
+     public Ford(int cylinders, String name){
+        super(cylinders, name);
+    }
+    @Override
+    public String startEngine(){
+        return "Starting Engine Ford";
+    }
+    @Override
+    public String accelerate(){
+        return "Accelerating Ford";
+    }
+    @Override
+    public String brake(){
+        return "Braking Car Ford";
+    }
+}

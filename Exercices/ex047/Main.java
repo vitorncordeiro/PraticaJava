da75e0d5e6120778f76b7c0a4d1414273b76733b
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-31 19:03:00 -0300

    Update Main.java

--- Exercices/ex047/Main.java
+++ Exercices/ex047/Main.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        String[] nomes;
+        String nome;
+        int quantidadeNomes;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a quantidade de nomes: ");
+        quantidadeNomes = sc.nextInt();
+        nomes = new String[quantidadeNomes];
+        for (int i = 0; i < quantidadeNomes; i++) {
+            System.out.println("Digite o nome: ");
+            nomes[i] = sc.next();
+        }
+        for (int i = 0; i < quantidadeNomes; i++) {
+            System.out.println(nomes[i]);
+        }
+
+
+    }
+}

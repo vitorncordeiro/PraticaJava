249e1b1707f0410a05f5c37140cfff9a1bc67e47
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-31 17:26:10 -0300

    Update Car.java

--- Exercices/ex046/Car.java
+++ Exercices/ex046/Car.java
@@ -1,1 +1,21 @@
+public abstract class Car {
+   private String name;
+   private boolean engine;
+
+   public Car(String name){
+       this.name = name;
+   }
+    public String startEngine(){
+        return "Starting engine" + this.getClass().getSimpleName();
+    }
+   public String drive(){
+        return "Driving";
+   }
+   protected void runEngine(boolean isEngineOn){
+       this.engine = isEngineOn;
+   }
+
+
+}
+
 

2b5aa2887bda984bdb43a2956f8739cd4fa14675
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-31 17:26:18 -0300

    Update GasPoweredCar.java

--- Exercices/ex046/GasPoweredCar.java
+++ Exercices/ex046/GasPoweredCar.java
@@ -1,1 +1,11 @@
+public class GasPoweredCar extends Car {
+    private double avgKmPerLitre;
+    private int cylinders;
+
+    public GasPoweredCar(String name, int cylinders) {
+        super(name);
+        this.cylinders = cylinders;
+    }
+
+}
 

801cadad6a940e418f5098fe2573d63bb0da5bf7
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-07-31 17:26:25 -0300

    Update ElectricCar.java

--- Exercices/ex046/ElectricCar.java
+++ Exercices/ex046/ElectricCar.java
@@ -1,1 +1,11 @@
+public class ElectricCar extends Car{
+    private double avgKmPerCharge;
+    private int batterySize;
+
+    public ElectricCar(String name, int batterySize) {
+        super(name);
+        this.batterySize = batterySize;
+    }
+
+}
 

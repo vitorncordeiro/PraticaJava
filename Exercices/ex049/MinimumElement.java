a3c863502992716582900091a27c0b4be8f7da0e
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-04 20:48:48 -0300

    Update MinimumElement.java

--- Exercices/ex049/MinimumElement.java
+++ Exercices/ex049/MinimumElement.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class MinimumElement {
+    private static int readInteger(){
+        Scanner sc = new Scanner(System.in);
+        int myQuantity = sc.nextInt();
+        return myQuantity;
+    }
+    private static int[] readElements(int quantity){
+        int[] intArray = new int[quantity];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < quantity; i++){
+            intArray[i] = sc.nextInt();
+        }
+        return intArray;
+    }
+    
+    private static int findMin(int[] myArray){
+        int minNum = myArray[0];
+        for(int i = 0; i < myArray.length; i++){
+            if(minNum > myArray[i]){
+                minNum = myArray[i];
+            }
+        }
+        return minNum;
+    }
+}

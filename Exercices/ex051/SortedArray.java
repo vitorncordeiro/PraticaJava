09cb62f1965f90dd6fdfa2145f47e770b892750c
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-05 22:52:32 -0300

    Create SortedArray.java

---
+++ Exercices/ex051/SortedArray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SortedArray {
+
+    public static int[] getIntegers(int size){
+        Scanner sc = new Scanner(System.in);
+        int[] unsortedArray = new int[size];
+        for(int i = 0; i < size; i++){
+            unsortedArray[i] = sc.nextInt();
+        }
+        return unsortedArray;
+    }
+    public static void printArray(int[] array){
+        for(int i : array){
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] unsortedArray){
+        Arrays.sort(unsortedArray);
+        int[] lastArray = new int[unsortedArray.length];
+        int j = unsortedArray.length - 1;
+        for(int i = 0; i < lastArray.length; i++){
+            lastArray[i] = unsortedArray[j];
+            j --;
+        }
+        return lastArray;
+    }
+}
+    
+ 

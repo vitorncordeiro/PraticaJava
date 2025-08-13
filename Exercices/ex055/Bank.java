debc20c4d237936fd1936dcc0ce528e4853fb85c
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-13 11:31:13 -0300

    Update Bank.java

--- Exercices/ex055/Bank.java
+++ Exercices/ex055/Bank.java
@@ -16,20 +16,19 @@ public class Bank {
         return true;
     }
     public boolean addCustomer(String branchName, String customerName, double inicialT){
-        Branch myB = new Branch(branchName);
-        
-        if(myB.newCustomer(customerName, inicialT)){
-            return true;
+        Branch existBranch = findBranch(branchName);
+        if(existBranch != null){
+            return existBranch.newCustomer(customerName, inicialT);
         }else{
             return false;
         }
         
     }
-    public boolean addCustomerTransaction(String branchName, String customerName, double transaction){
-        Branch myB = new Branch(branchName);
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount){
+        Branch existBranch = findBranch(branchName);
         
-        if(myB.addCustomerTransaction(customerName, transaction)){
-            return true;
+        if(existBranch != null){
+            return existBranch.addCustomerTransaction(customerName, amount);
         }
         return false;
         
@@ -38,14 +37,29 @@ public class Bank {
     
     private Branch findBranch(String branchName){
         for(int i = 0; i < branches.size(); i++){
-            if(branches[i].getName() == branchName){
-                return branches[i];
+            if(branches.get(i).getName() == branchName){
+                return branches.get(i);
             }
         }
         return null;
     }
     public boolean listCustomers(String branchName, boolean printTransactions){
-        
+        Branch myB = findBranch(branchName);
+        if(myB != null){
+
+            System.out.println("Customer details for branch " + myB.getName());
+            for(int i = 0; i < myB.getCustomers().size(); i++){
+                System.out.println("Customer: " + myB.getCustomers().get(i).getName() + "[" + (i + 1) + "]");
+                if(printTransactions){
+                    System.out.println("Transactions");
+                    for(int j = 0; j < myB.getCustomers().get(i).getTransactions().size(); j++){
+                        System.out.println("[" + (j + 1) + "] Amount " +  myB.getCustomers().get(i).getTransactions().get(j));
+                }
+                }
+            }
+            return true;
+        }
+        return false;
     }
     
 }

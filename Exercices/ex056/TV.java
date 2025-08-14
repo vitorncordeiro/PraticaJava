e634bc85469d5c46afcad738b3cf581fc682943c
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-14 16:52:56 -0300

    Create TV.java

---
+++ Exercices/ex056/TV.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+public class TV  extends  ProductForSale{
+    public TV(String type, double price, String description){
+        super(type, price, description);
+    }
+    public void showDetails(){
+        System.out.println("This product is a(an) " + this.type + ", the unit price is " + this.getPrice() + " and its description is: " + this.getDescription());
+    }
+
+}

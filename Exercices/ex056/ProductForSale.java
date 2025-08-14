f56bd0bc6b1d8c62151e2f38ca8bdb2464234185
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-14 16:52:47 -0300

    Update ProductForSale.java

--- Exercices/ex056/ProductForSale.java
+++ Exercices/ex056/ProductForSale.java
@@ -1,1 +1,24 @@
+package dev.lpa;
+
+public abstract class ProductForSale {
+    protected String type;
+    private double price;
+    private String description;
+
+    public ProductForSale(String type, double price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getSalesPrice(int quantity) {
+        return quantity * price;
+    }
+    public abstract void showDetails();
+}
 

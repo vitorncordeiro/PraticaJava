59b00f0165eb18d8e2e3d69db0d221a046c72895
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-08-19 17:02:18 -0300

    Update Main.java

--- Exercices/ex058/Main.java
+++ Exercices/ex058/Main.java
@@ -1,1 +1,24 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Mappable jardimBotanico = new Building("Jardim Botânico", "Green Push pin");
+        Mappable.printProperties(jardimBotanico);
+
+        Mappable jardimBotanicoPipes = new UtilityLine("Jardim Botânico Pipes", "Red Line", "Plumbing");
+        Mappable.printProperties(jardimBotanicoPipes);
+
+        System.out.println("--------------------------------------------");
+
+        List<Mappable> places = new ArrayList<>();
+        places.add(jardimBotanico);
+        places.add(jardimBotanicoPipes);
+        Mappable.printProperties(places);
+        
+    }
+}
+
 

a1c09a15e3f2372118d71acbba0295557ccb1aaf
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-09-02 22:14:50 -0300

    Update Main.java

--- Exercices/ex070/Main.java
+++ Exercices/ex070/Main.java
@@ -1,1 +1,9 @@
+package code;
 
+public class Main {
+    public static void main(String[] args) {
+        Theatre teste = new Theatre("Vitor", 10, 100);
+        teste.getSeats().forEach(System.out::println);
+    }
+
+}

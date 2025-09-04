3abd64d37f98ad47ae7dc6bcae61d7e00b51ae54
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-09-03 22:44:25 -0300

    Update Main.java

--- Exercices/ex070/Main.java
+++ Exercices/ex070/Main.java
@@ -2,8 +2,18 @@ package code;
 
 public class Main {
     public static void main(String[] args) {
-        Theatre teste = new Theatre("Vitor", 10, 100);
+        Theatre teste = new Theatre("Vitor", 5, 13);
         teste.getSeats().forEach(System.out::println);
+
+        teste.reserve('A', 'B', 4);
+        teste.getSeats().forEach(System.out::println);
+
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+
+        teste.reserve(3);
+        teste.getSeats().forEach(System.out::println);
+
+
     }
 
 }
